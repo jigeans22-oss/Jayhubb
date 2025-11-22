e7cf2512bb16fdc804ec0f6921e0a4b356f08b2c
jigeans22-oss <jigeans22@gmail.com>
2025-11-22 18:57:06 -0500

    Update loading title and key in Tb3.lua

--- Tb3.lua
+++ Tb3.lua
@@ -4,7 +4,7 @@ local Rayfield = loadstring(game:HttpGet('https://[Log in to view URL]'))()
 local Window = Rayfield:CreateWindow({
    Name = "✔THA BRONX 3",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "MADE BY CODYISTHETITS",
+   LoadingTitle = "MADE BY Haxzo",
    LoadingSubtitle = "TB3",
    Theme = "Ocean", -- Check https://[Log in to view URL]
 
@@ -31,7 +31,7 @@ local Window = Rayfield:CreateWindow({
       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"cdy"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"Haxzo"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
 
